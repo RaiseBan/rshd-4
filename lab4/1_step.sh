5df2d212b301d91512e978f2b20a5b522768f09f
Sergey <shornikov1407@gmail.com>
2025-05-11 03:44:38 +0300

    1 step ready

--- lab4/1_step.sh
+++ lab4/1_step.sh
@@ -7,7 +7,7 @@ mkdir database
 initdb -D database
 
 cd database
-cat >> postgresql.conf << EOF
+cat > postgresql.conf << EOF
 listen_addresses = '*'
 port = 5433
 wal_level = replica
@@ -18,8 +18,8 @@ EOF
 
 cat > pg_hba.conf << EOF
 local   all             all                                     trust
-host    all             all             0.0.0.0/0               md5
-host    replication     all             0.0.0.0/0               md5
+host    all             all             0.0.0.0/0               password
+host    replication     all             0.0.0.0/0               password
 EOF
 
 cd ..
@@ -40,7 +40,7 @@ docker exec -it -u postgres postgres-standby bash
 echo 'export PATH=/usr/lib/postgresql/14/bin:$PATH' >> ~/.bashrc
 
 
-# export PATH=/usr/lib/postgresql/14/bin:$PATH
+export PATH=/usr/lib/postgresql/14/bin:$PATH
 
 
 mkdir -p ~/.ssh
@@ -56,12 +56,25 @@ PGPASSWORD=123 pg_basebackup -h 172.20.0.10 -p 5433 -U replicator -D ~/database 
 
 pg_ctl -D ~/database start
 
+exit
 
 docker exec -it -u postgres postgres-master bash
 export PATH=/usr/lib/postgresql/14/bin:$PATH
 psql -p 5433 -c "SELECT * FROM pg_stat_replication;"
 
 # on pgpool-clien node:
+
+
+
+
+cat > /etc/pgpool2/pool_hba.conf << EOF
+# TYPE  DATABASE    USER        CIDR-ADDRESS          METHOD
+local   all         all                               trust
+host    all         all         127.0.0.1/32          password
+host    all         all         ::1/128               password
+host    all         all         0.0.0.0/0             password
+EOF
+
 
 cat > /etc/pgpool2/pgpool.conf << EOF
 listen_addresses = '*'
@@ -72,19 +85,55 @@ backend_port0 = 5433
 backend_weight0 = 1
 backend_data_directory0 = '/var/lib/postgresql/database'
 backend_flag0 = 'ALLOW_TO_FAILOVER'
-backend_application_name0 = 'master'
 
 backend_hostname1 = '172.20.0.11'
 backend_port1 = 5433
 backend_weight1 = 1
 backend_data_directory1 = '/var/lib/postgresql/database'
 backend_flag1 = 'ALLOW_TO_FAILOVER'
-backend_application_name1 = 'standby'
+
+backend_clustering_mode = 'streaming_replication'
+
+log_connections = on
+log_hostname = on
+log_statement = on
+log_per_node_statement = on
+log_min_messages = debug5
+log_error_verbosity = verbose
+
+EOF
+
+
+# test
+
+cat > /etc/pgpool2/pgpool.conf << EOF
+
+listen_addresses = '*'
+port = 5432
+
+backend_hostname0 = '172.20.0.10'
+backend_port0 = 5433
+backend_weight0 = 1
+backend_data_directory0 = '/var/lib/postgresql/database'
+backend_flag0 = 'ALLOW_TO_FAILOVER'
+
+backend_hostname1 = '172.20.0.11'
+backend_port1 = 5433
+backend_weight1 = 1
+backend_data_directory1 = '/var/lib/postgresql/database'
+backend_flag1 = 'ALLOW_TO_FAILOVER'
 
 num_init_children = 32
 max_pool = 4
-backend_clustering_mode = 'streaming_replication'
+
+backend_clustering_mode = 'streaming_replication'       
 load_balance_mode = on
+master_slave_mode = on
+master_slave_sub_mode = stream
+statement_level_load_balance = on
+disable_load_balance_on_write = off
+
+
 
 sr_check_period = 10
 sr_check_user = 'replicator'
@@ -92,13 +141,37 @@ sr_check_password = '123'
 sr_check_database = 'postgres'
 
 enable_pool_hba = off
-EOF
-cd /etc/pgpool2
-pg_md5 -m -u buddy 123
 
+log_min_messages = debug1
+log_per_node_statement = on
+pool_passwd = '/etc/pgpool2/pgpool.conf'
+EOF
+
+
+
+enable_pool_hba = on
+backend_clustering_mode = 'streaming_replication'
 
 mkdir -p /var/run/pgpool
 
 pgpool -n -f /etc/pgpool2/pgpool.conf > /var/log/pgpool.log 2>&1 &
+
 ps aux | grep pgpool
 netstat -nlp | grep 5432
+
+# test balancing and replication
+psql -h localhost -p 5433 -U buddy -d postgres
+
+CREATE TABLE test_replication (
+    id SERIAL PRIMARY KEY,
+    data TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+INSERT INTO test_replication (data) VALUES ('Test data 1'), ('Test data 2');
+
+# check results of replication:
+
+docker exec -u postgres postgres-master psql -p 5433 -c "SELECT * FROM test_replication;"
+
+docker exec -u postgres postgres-standby psql -p 5433 -c "SELECT * FROM test_replication;"
+
